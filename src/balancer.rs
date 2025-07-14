beaa908fa47fc209a0580bc99d6f974dfcf9a5ee
qxb3 <qxbthree@gmail.com>
2025-07-14 16:35:00 +0800

    done?

--- src/balancer.rs
+++ src/balancer.rs
@@ -1,7 +1,9 @@
 use std::{collections::HashMap, sync::Arc};
 
 use tokio::{
-    io::AsyncReadExt, net::{TcpListener, TcpStream}, sync::Mutex
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::{TcpListener, TcpStream},
+    sync::Mutex,
 };
 
 type LBData = Arc<Mutex<HashMap<String, u32>>>;
@@ -18,52 +20,68 @@ pub async fn start_load_balancer(address: String) {
     // Inside a scope for the lock to be freed.
     {
         let mut data_ref = data.lock().await;
-        data_ref.insert("127.0.0.1:9001".to_string(), 0);
-        data_ref.insert("127.0.0.1:9002".to_string(), 0);
-        data_ref.insert("127.0.0.1:9003".to_string(), 0);
-        data_ref.insert("127.0.0.1:9004".to_string(), 0);
-        data_ref.insert("127.0.0.1:9005".to_string(), 0);
-        data_ref.insert("127.0.0.1:9006".to_string(), 0);
-        data_ref.insert("127.0.0.1:9007".to_string(), 0);
-        data_ref.insert("127.0.0.1:9008".to_string(), 0);
+        for i in 1..=8 {
+            data_ref.insert(format!("127.0.0.1:900{i}"), 0);
+        }
     }
 
     loop {
-        let (stream, _) = listener
-            .accept()
-            .await
-            .expect("Failed to receive incoming request");
+        let (stream, _) = listener.accept().await.unwrap();
 
         let data = Arc::clone(&data);
+
         tokio::spawn(async move {
             balance(stream, data).await;
         });
     }
 }
 
-async fn balance(mut client_stream: TcpStream, data: LBData) {
-    let least_load_address = get_least_load_address(data).await;
+async fn balance(client_stream: TcpStream, data: LBData) {
+    let least_load_address = get_least_load_address(Arc::clone(&data)).await;
+    let backend_stream = TcpStream::connect(&least_load_address).await.unwrap();
 
-    let mut backend_stream = TcpStream::connect(least_load_address)
-        .await
-        .expect("Failed to bind to backend");
+    let (mut csr, mut csw) = client_stream.into_split();
+    let (mut bsr, mut bsw) = backend_stream.into_split();
 
-    let (mut csr, mut csw) = client_stream.split();
-    let (mut bsr, mut bsw) = backend_stream.split();
+    // client -> backend.
+    let cb_handle = tokio::spawn(async move {
+        loop {
+            let mut request = [0; 1024];
+            match csr.read(&mut request).await {
+                Ok(0) => break,
+                Ok(n) => bsw.write_all(&request[..n]).await.unwrap(),
+                Err(_) => break,
+            }
+        }
+    });
 
-    let forward_handle = tokio::io::copy(&mut csr, &mut bsw);
-    let backward_handle = tokio::io::copy(&mut bsr, &mut csw);
+    // backend -> client.
+    let bc_handle = tokio::spawn(async move {
+        loop {
+            let mut response = [0; 1024];
+            match bsr.read(&mut response).await {
+                Ok(0) => break,
+                Ok(n) => csw.write_all(&response[..n]).await.unwrap(),
+                Err(_) => break,
+            }
+        }
+    });
 
-    tokio::try_join!(forward_handle, backward_handle)
-        .expect("Failed to join handles");
+    let _ = tokio::join!(cb_handle, bc_handle);
+
+    let mut data_ref = data.lock().await;
+    let load = data_ref.get_mut(&least_load_address).unwrap();
+    *load -= 1;
 }
 
 async fn get_least_load_address(data: LBData) -> String {
     let mut data_ref = data.lock().await;
-    let (address, _) = data_ref
+    let (address, load) = data_ref
         .iter_mut()
         .min_by_key(|(_, v)| **v)
         .expect("Failed to find the backend with the least load");
+
+    *load += 1;
 
     address.to_string()
 }
--- src/server.rs
+++ src/server.rs
@@ -1,7 +1,10 @@
-use std::{ops::{AddAssign, SubAssign}, sync::Arc, thread, time::Duration};
+use std::{
+    ops::{AddAssign, SubAssign},
+    sync::Arc,
+};
 
 use tokio::{
-    io::AsyncWriteExt,
+    io::{AsyncReadExt, AsyncWriteExt},
     net::{TcpListener, TcpStream},
     sync::Mutex,
 };
@@ -31,24 +34,37 @@ pub async fn start_server(address: String) {
 async fn handle_request(mut stream: TcpStream, load: Arc<Mutex<i32>>) {
     load.lock().await.add_assign(1);
 
-    stream
-        .write_all(b"Started processing\n")
-        .await
-        .expect("Failed to send start confirmation");
-
-    // Simulates some heavy computation.
-    let random_sleep = rand::random_range(0..=1);
-    thread::sleep(Duration::from_secs(random_sleep));
-
-    stream
-        .write_all(b"Done processing\n")
-        .await
-        .expect("Failed to send done confirmation");
-
-    {
-        let mut load_ref = load.lock().await;
-        println!("Processed: {load_ref}");
-
-        *load_ref -= 1;
+    let mut request = Vec::new();
+    loop {
+        let mut buf = [0; 1024];
+        match stream.read(&mut buf).await {
+            Ok(0) => break,
+            Ok(n) => {
+                request.extend_from_slice(&buf[..n]);
+                if buf[..n].ends_with(b"\0") {
+                    request.pop(); // Removes the null terminator.
+                    break;
+                }
+            }
+            Err(_) => break,
+        }
     }
+
+    let target_number = String::from_utf8(request).unwrap().parse::<u64>().unwrap();
+
+    let result = fib(target_number);
+
+    stream
+        .write_all(format!("{result}").as_bytes())
+        .await
+        .unwrap();
+
+    println!("Done Calculation: {}", load.lock().await);
+
+    load.lock().await.sub_assign(1);
+}
+
+// Calculate fibonacci.
+fn fib(n: u64) -> u64 {
+    if n <= 1 { n } else { fib(n - 1) + fib(n - 2) }
 }
